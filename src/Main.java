2b83ff2fa0fbde33e449f4c0d20114535c2e6df8
svdeeq21 <sadiqmshehu21@gmail.com>
2025-03-12 16:37:14 +0100

    first batch

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+
+    public static void main(String[] args) {
+        Something app = new Something();
+        app.setName("My first app");
+        System.out.println(app.getName());
+    }
+}
