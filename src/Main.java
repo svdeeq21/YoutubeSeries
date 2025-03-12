6be6032d6d909696fe1f1faace1f0fe3d6a18903
svdeeq21 <sadiqmshehu21@gmail.com>
2025-03-12 17:27:09 +0100

    Played around with some static local and instance variables in java.

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,17 @@
 public class Main {
-
-    public static void main(String[] args) {
-        Something app = new Something();
-        app.setName("My first app");
-        System.out.println(app.getName());
+    public static String name = "John";
+    public String names;
+    public int age;
+    public Main(String names, int age) {
+        this.names = names;
+        this.age = age;
     }
+    public static void main(String[] args) {
+      String name = "Doe";
+      System.out.println(name);  
+      System.out.println(Main.name);
+      Main obj = new Main("Jane", 30);
+      System.out.println(obj.age);
+    }
+
 }
